17c5f74bd9309191c06edc78b08672a62a468f0a
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-04-29 16:36:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+const prices = document.querySelectorAll('[data-ns-test="prices"]');
+
+// Calculate the sum of all prices
+let totalPrice = 0;
+prices.forEach(price => {
+  totalPrice += parseFloat(price.textContent);
+});
+
+// Create a new row in the HTML table to display the total price
+const table = document.querySelector('table');
+const newRow = table.insertRow();
+const newCell = newRow.insertCell();
+
+newCell.setAttribute('data-ns-test', 'grandTotal');
+newCell.textContent = totalPrice.toFixed(2);
+
+
